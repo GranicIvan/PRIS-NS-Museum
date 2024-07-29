825659e6f0e5bbdd1cc4bae08390e380fe73e366
“TamaraT99” <tamaratodorovic16@gmail.com>
2024-07-29 14:36:41 +0200

    remove delo model dto 2

--- vodic-backend/src/main/java/com/example/demo/control/DeloController.java
+++ vodic-backend/src/main/java/com/example/demo/control/DeloController.java
@@ -6,22 +6,6 @@ import java.util.stream.Collectors;
 
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-package com.example.demo.controller;
-
-import java.util.List;
-import java.util.Optional;
-
-
-import org.apache.coyote.BadRequestException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,6 +23,7 @@ import com.example.demo.model.Period;
 import com.example.demo.repository.DeloRepository;
 import com.example.demo.repository.LicnostRepository;
 import com.example.demo.repository.PeriodRepository;
+
 
 @RestController
 @RequestMapping("/delo")
